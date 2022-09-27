be72de674d454517e1bde78f1704b84a93803ed9
meremetis.p <mere.p.w_@hotmail.com>
2022-09-27 18:35:30 +0300

    Initial Commit

---
+++ src/gr/aueb/sev/vars/intsApp.java
@@ -0,0 +1,11 @@
+package gr.aueb.sev.vars;
+
+public class intsApp {
+    public static void main(String[] args) {
+         int num = 10;
+         int num2 =20;
+         int result = num + num2;
+
+        System.out.println("result : " + result);
+    }
+}
